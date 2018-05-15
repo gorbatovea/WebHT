d253eaf29c6ba3f0700069ea9e535902f142a39e
gorbatov <gorbatov.eg@gmail.com>
2018-05-15 10:29:06 +0300

    Bug fixes in Utils class

--- src/main/java/com/utils/Utils.java
+++ src/main/java/com/utils/Utils.java
@@ -2,6 +2,8 @@ package com.utils;
 
 public class Utils {
     public static boolean isNumeric(String str) {
+        if (str == null) return false;
+        if (str.length() < 1) return false;
         for (char c : str.toCharArray()) {
             if (!Character.isDigit(c)) return false;
         }
